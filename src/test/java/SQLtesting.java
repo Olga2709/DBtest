2f409afb66002032de15de38d8f8df0c115fb371
Olga2709 <olga.shvager@gmail.com>
2020-01-28 14:10:54 +0200

    code sample for simple DB test

--- src/test/java/SQLtesting.java
+++ src/test/java/SQLtesting.java
@@ -1,6 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 import java.sql.*;
 public class SQLtesting {
     public static void main(SQLtesting[] args) {
+
+        //TestData
+        String emailaddress = "blahblah";
+        String zipcode = "blahblah";
+        String firstname = "blahblah";
+        String lastname = "blahblah";
+
+
+        //Input testdData using Selenium
+        WebDriver driver = new FirefoxDriver();
+        driver.get("any_url");
+        driver.findElement(By.xpath("___________")).sendKeys(emailaddress);
+        driver.findElement(By.xpath("___________")).sendKeys(zipcode);
+        driver.findElement(By.xpath("___________")).sendKeys(firstname);
+        driver.findElement(By.xpath("___________")).sendKeys(lastname);
+        driver.findElement(By.xpath("signupbutton"));
+
+
+        //Connecting to DataBase
         Connection con = null;
         String url = "jdbs:mysql://localhost:3306/";
         String databaseName = "name_of_database";
@@ -18,8 +41,25 @@ public class SQLtesting {
             result.next();
             //Column of the tables
             System.out.println(result.getString("emailaddress"));
-            System.out.println(result.getString("emailaddress"));
-            System.out.println(result.getString("emailaddress"));
+            System.out.println(result.getString("zipcode"));
+            System.out.println(result.getString("firstname"));
+            System.out.println(result.getString("lastname"));
+
+
+
+            //DataBase Testing
+
+            if(! result.getString("emailaddress").equals(emailaddress));
+            System.out.println("Wrong email address");
+
+            if(! result.getString("zipcode").equals(zipcode));
+            System.out.println("Wrong zipcode ");
+
+            if(! result.getString("firstname").equals(firstname));
+            System.out.println("Wrong firstname");
+
+            if(! result.getString("lastname").equals(lastname));
+            System.out.println("Wrong lastname");
 
 
 
